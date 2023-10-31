ccb7d77489c15a0eae8d5e0f2f083144ec422c1f
Saydullin <saydullindev@gmail.com>
2023-10-31 18:50:48 +0300

    WindowProc added

--- SP_lab_1.cpp
+++ SP_lab_1.cpp
@@ -43,4 +43,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	return msg.wParam;
 }
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	RECT clientRect;
+	GetClientRect(hwnd, &clientRect);
+	int wheelParam = 0;
+	static RECT rect = { 50, 50, 100, 100 };
+	int step = 5;
+	int xPos = 0;
+	int yPos = 0;
+	switch (uMsg) {
+	case WM_CREATE:
+		return 0;
+	case WM_PAINT:
+		return 0;
+	case WM_MOUSEWHEEL:
+		xPos = rect.left;
+		yPos = rect.top;
+		wheelParam = GET_WHEEL_DELTA_WPARAM(wParam);
+		SetRect(&rect, xPos, yPos, xPos + 50, yPos + 50);
+		InvalidateRect(hwnd, nullptr, TRUE);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
 
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+}
